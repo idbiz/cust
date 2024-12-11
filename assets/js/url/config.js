ff02ab6968a14b79eac0db779818439151e4e539
GilangAndhika <gryzlegrizz@gmail.com>
2024-12-11 20:14:42 +0700

    Update base API URL in config.js to simplify endpoint structure

--- assets/js/url/config.js
+++ assets/js/url/config.js
@@ -1,6 +1,6 @@
 import { folderPath } from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
-const baseAPIURL = 'https://asia-southeast2-awangga.cloudfunctions.net/idbiz/data/user'
+const baseAPIURL = 'https://asia-southeast2-awangga.cloudfunctions.net/idbiz'
 
 export const conf = {
     user: `${baseAPIURL}/data/user`,
