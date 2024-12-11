dd572adadb5a693694bca6410bea28556cad5ceb
GilangAndhika <gryzlegrizz@gmail.com>
2024-12-11 20:14:17 +0700

    Update base API URL for user data in config.js

--- assets/js/url/config.js
+++ assets/js/url/config.js
@@ -1,6 +1,6 @@
 import { folderPath } from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
-const baseAPIURL = 'https://id.biz.id'
+const baseAPIURL = 'https://asia-southeast2-awangga.cloudfunctions.net/idbiz/data/user'
 
 export const conf = {
     user: `${baseAPIURL}/data/user`,
